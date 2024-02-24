c4c5d03a6653a89f5f15e4023df58bef9854a30e
rilking1 <kozakevycr2@gmail.com>
2024-02-25 00:29:28 +0200

    Add diagram for Home page. Complete 1.5 ( Maybe =) )

---
+++ WebApplication4/Controllers/ChartsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using WebApplication4.Data;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    [Route("api/[controller]")] // /api/charts/...
+    [ApiController]
+    public class ChartsController : ControllerBase
+    {
+        private readonly DbcoursesContext context;
+
+        public ChartsController(DbcoursesContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("countByDifLvl")]
+        public async Task <IActionResult> GetCountByDifLvlAsync() 
+        {
+
+            {
+                var result = await context.Courses
+                    .GroupBy(category => category.DifficultyLevel.DifLevel)
+                    .Select(categoryGroup => new drawСountByDifLvlChart(categoryGroup.Key.ToString(), categoryGroup.Count()))
+                    .ToListAsync();
+                return new JsonResult(result);
+
+            };
+        }
+        [HttpGet("countByCategory")]
+        public async Task<IActionResult> GetCountByCategoryAsync()
+        {
+            var result = await context.Courses
+                    .GroupBy(course => course.Category.Category1)
+                    .Select(categoryGroup => new drawСountCategoryChart( categoryGroup.Key.ToString(), categoryGroup.Count() ))
+                    .ToListAsync();
+            return new JsonResult(result);
+        }
+
+    }
+}
---
+++ WebApplication4/Models/CountByDifLvlResponceItem.cs
@@ -0,0 +1,3 @@
+﻿namespace WebApplication4.Models;
+
+public record class drawСountByDifLvlChart(string DifLvl, int Count);
